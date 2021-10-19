37b1b9bdeddaace06fdc430eead27821200c4f06
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-10-19 23:01:09 +0300

    update

---
+++ inheritanc/lib/src/main/java/inheritanc/Review.java
@@ -0,0 +1,46 @@
+package inheritanc;
+
+
+public class Review {
+    private String body;
+    private String author;
+    private int star;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public Review(String body, String author, int star){
+        this.body = body;
+        this.author =author;
+        this.star = star;
+    }
+    public String toString(){
+        String comments = " comments : " + this.body + " / "+ " name : " + this.author + " start * " + this.star;
+        return comments;
+    }
+    public String test (){
+        String comments = "from comments";
+        return comments;
+    }
+}
--- inheritance/lib/src/main/java/inheritance/Review.java
+++ inheritance/lib/src/main/java/inheritance/Review.java
@@ -1,11 +1,35 @@
 package inheritance;
 
 public class Review {
-    String body;
-    String author;
-    int star;
+    private String body;
+    private String author;
+    private int star;
 
-    public Review(String body,String author,int star){
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public Review(String body, String author, int star){
         this.body = body;
         this.author =author;
         this.star = star;
